1b9179b481fe12a3e044292d7b0608edb89d2281
leon-carvalho <leonardoscc15@gmail.com>
2020-09-01 12:20:36 -0300

    create ActionTypes

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -9,6 +9,7 @@ import {
 import api from "../../../services/api";
 import { IState } from "../..";
 import { AxiosResponse } from "axios";
+import { ActionTypes } from "./types";
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
 
@@ -40,5 +41,5 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
 }
 
 export default all([
-  takeLatest("@cart/ADD_PRODUCT_TO_CART_REQUEST", checkProductStock),
+  takeLatest(ActionTypes.addProductToCartRequest, checkProductStock),
 ]);
--- src/store/modules/cart/types.ts
+++ src/store/modules/cart/types.ts
@@ -1,3 +1,9 @@
+export enum ActionTypes {
+  addProductToCartRequest = "@cart/ADD_PRODUCT_TO_CART_REQUEST",
+  addProductToCartSuccess = "@cart/ADD_PRODUCT_TO_CART_SUCCESS",
+  addProductToCartFailure = "@cart/ADD_PRODUCT_TO_CART_FAILURE",
+}
+
 export interface IProduct {
   id: number;
   title: string;
